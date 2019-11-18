b3bfee2a7aa05d66eb32d81d7b1a1e43fce0224c
William Blacknall <williamblacknall@gmail.com>
2019-11-18 08:15:28 -0500

    More styling. The styling is now as good as it's probably going to get. Got the Navigation component to stand next to page content. Now working to make it actually functional

--- src/Components/Brazil/Brazil.js
+++ src/Components/Brazil/Brazil.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./Brazil.css";
 import BrazilContent from "./Brazil.jsx";
-import Navigation from "../Navigation/Navigation"
 
 export default class Brazil extends React.Component {
 	render() {
 		return (<>
 		<BrazilContent />
-		<Navigation />
 		</>
 			)
 	}
--- src/Components/Brazil/Brazil.jsx
+++ src/Components/Brazil/Brazil.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import RioRedeemer from "./images/Christ-the-Redeemer-980x613.jpg";
-import RioEscalera from "./images/Escalera coloridas-Selaron5-.jpeg";
+import RioRedeemer from "./images/riodejaneiro.jpg";
+import Centro from "./images/centrobrazil.jpg";
 import BahiaSalvador from "./images/salvador-brazil-postcard.webp";
-import BahiaStreet from "./images/Bahia-street.jpg";
+import BahiaStreet from "./images/bahiacity.jpg";
 import FozDoIguacu from "./images/foz-iguazufalls.jpg";
-import SaoPaulo from "./images/saopaulo.jpg"
-import Brasilia from "./images/brasilia.jpg"
-import Agronomica from "./images/agronomica.jpg"
+import SaoPaulo from "./images/saopaulo.jpg";
+import Brasilia from "./images/brasilia.jpg";
+import Agronomica from "./images/agronomica.jpg";
+import Navigation from "../Navigation/Navigation";
 
 export default function BrazilContent() {
 	return (
@@ -63,109 +64,133 @@ export default function BrazilContent() {
 				</ul>
 			</nav>
 			<main className="brazil-background">
-			<div className="tile is-ancestor ">
-				<div className="tile is-parent">
-					<div className="tile is-child">
-						<figure>
-							<img
-								className="imagecontent"
-								src={RioRedeemer}
-								alt="Rio De Janerio"
-								id="Rio de Janerio"
-							/>
-							<figcaption className="subtitle">Rio De Janerio</figcaption>
-						</figure>
+				<div class="tile is-ancestor">
+					<div class="tile is-vertical is-8">
+						<div class="tile is-parent">
+							<article class="tile is-child">
+								<div class="content">
+									<figure>
+										<img
+											className="imagecontent"
+											src={Agronomica}
+											alt="Agronomica"
+											id="Agronomica"
+										/>
+										<figcaption className="subtitle">
+											Agronomica
+										</figcaption>
+									</figure>
+								</div>
+							</article>
+							<article className="tile is-child">
+								<figure>
+									<img
+										className="imagecontent"
+										src={SaoPaulo}
+										alt="Sao Paulo"
+										id="SaoPaulo"
+									/>
+									<figcaption className="subtitle">
+										Sao Paulo
+									</figcaption>
+								</figure>
+							</article>
+						</div>
+						<div className="tile is-parent">
+							<article className="tile is-child">
+								<div className="content">
+									<figure>
+										<img
+											className="imagecontent"
+											src={Brasilia}
+											alt="Brasilia"
+											id="Brasilia"
+										/>
+										<figcaption className="subtitle">
+											Brasilia
+										</figcaption>
+									</figure>
+								</div>
+							</article>
+							<article className="tile is-child">
+								<div className="content">
+								<figure>
+									<img
+										className="imagecontent"
+										src={FozDoIguacu}
+										alt="Foz do Iguacu"
+										id="Foz do Iguacu"
+									/>
+									<figcaption className="subtitle">
+										Foz do Iguacu
+									</figcaption>
+								</figure>
+								</div>
+							</article>
+						</div>
+						<div className="tile is-parent">
+							<article className="tile is-child">
+								<figure>
+								<img
+									className="imagecontent"
+									src={RioRedeemer}
+									alt="Rio De Janerio"
+									id="Rio de Janerio"
+								/>
+								<figcaption className="subtitle">
+									Rio De Janerio
+								</figcaption>
+							</figure>
+							</article>
+							<article className="tile is-child">
+								<figure>
+								<img
+									className="imagecontent"
+									src={Centro}
+									alt="Centro"
+									id="Centro"
+								/>
+								<figcaption className="subtitle">Centro</figcaption>
+							</figure>
+							</article>
+						</div>
+						<div className="tile is-parent">
+							<article className="tile is-child">
+								<figure>
+								<img
+									className="imagecontent"
+									src={BahiaSalvador}
+									alt="Bahia"
+									id="Bahia"
+								/>
+								<figcaption className="subtitle">
+									Salvador
+								</figcaption>
+							</figure>
+							</article>
+							<article className="tile is-child">
+								<figure>
+								<img
+									className="imagecontent"
+									src={BahiaStreet}
+									alt="Bahia City"
+									id="Bahia City"
+								/>
+								<figcaption className="subtitle">
+									Bahia City
+								</figcaption>
+							</figure>
+							</article>
+						</div>
 					</div>
-					<div className="tile is-child">
-						<figure>
-							<img
-								className="imagecontent"
-								src={RioEscalera}
-								alt="Rio De Janerio"
-								id="Rio De Janerio"
-							/>
-						</figure>
+					<div class="tile is-parent">
+						<article class="tile is-child">
+							<div class="content">
+								<Navigation />
+							</div>
+						</article>
 					</div>
 				</div>
-			</div>
-			<div className="tile is-ancestor">
-				<div className="tile is-parent">
-					<div className="tile is-child">
-						<figure>
-							<img
-								className="imagecontent"
-								src={BahiaSalvador}
-								alt="Bahia"
-								id="Bahia"
-							/>
-							<figcaption className="subtitle">Salvador</figcaption>
-						</figure>
-					</div>
-					<div className="tile is-child">
-						<figure>
-							<img
-								className="imagecontent"
-								src={BahiaStreet}
-								alt="Bahia City"
-								id="Bahia City"
-							/>
-							<figcaption className="subtitle">Bahia City</figcaption>
-						</figure>
-					</div>
-				</div>
-			</div>
-			<div className="tile is-ancestor">
-				<div className="tile is-parent">
-					<div className="tile is-child">
-						<figure>
-						<img
-							className="imagecontent"
-							src={FozDoIguacu}
-							alt="Foz do Iguacu"
-							id="Foz do Iguacu"
-						/>
-						<figcaption className="subtitle">Foz do Iguacu</figcaption>
-						</figure>
-					</div>
-					<div className="tile is-child">
-						<figure>
-							<img
-							className="imagecontent"
-							src={SaoPaulo}
-							alt = "Sao Paulo"
-							id = "SaoPaulo"
-							/>
-							<figcaption className="subtitle">Sao Paulo</figcaption>
-						</figure>
-					</div>
-				</div>
-			</div>
-			<div className="tile is-ancestor">
-				<div className="tile is-parent">
-					<div className="tile is-child">
-						<figure>
-							<img
-							className="imagecontent"
-							src={Brasilia}
-							alt = "Brasilia"
-							id = "Brasilia"
-							/>
-							<figcaption className="subtitle">Brasilia</figcaption>
-						</figure>
-					</div>
-					<div className="tile is-child">
-						<figure>
-							<img
-							className="imagecontent"
-							src={Agronomica}
-							alt="Agronomica"
-							id = "Agronomica"
-							/>
-							<figcaption className="subtitle">Agronomica</figcaption>
-						</figure>
-					</div>
-				</div>
-			</div>
 			</main>
 		</React.Fragment>
 	);
