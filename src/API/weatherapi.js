d8c26123a4d7d5b36ee0f7f09ba44be5f300f6d0
William Blacknall <williamblacknall@gmail.com>
2019-11-19 09:25:06 -0500

    Finally got the API outputs to show up with the help of my good friend Christopher Kemp. Now I need to pass the props down to load each individual actual location

--- src/API/weatherapi.js
+++ src/API/weatherapi.js
@@ -2,4 +2,5 @@ import Axios from "axios";
 
 export default Axios.create({
 	baseURL: "https://api.openweathermap.org/data/2.5/",
+	
 });
--- src/Components/Navigation/Weather/Weather.js
+++ src/Components/Navigation/Weather/Weather.js
@@ -4,62 +4,92 @@ import weatherapi from "../../../API/weatherapi";
 
 class WeatherBox extends React.Component {
 	state = {
-		Location: null
+		location: null,
+		currentTemp: 0,
+		currentPrecip: 0,
+		currentHumid: 0,
+		currentWind: 0,
+		currentHigh: 0,
+		currentLow: 0,
+		secondDayTemp: 0,
+		thirdDayTemp: 0,
+		fourthDayTemp: 0,
+		fifthDayTemp: 0,
+		secondDayName: null,
+		thirdDayName: null,
+		fourthDayName: null,
+		fifthDayName: null
 	};
 
-	onImageClick = async location => {
+	componentDidMount = async location => {
 		const res = await weatherapi.get("/forecast", {
 			params: {
-				q: location,
+				q: "Atlanta",
 				units: "imperial",
 				APPID: "d5bfe8a65a2dce437fd2e6c635989395"
 			}
 		});
-		console.log(res.data.list)
-		const tomato = "Tomato"
-        //Gets the current temperature
-        const currentDayTemp = res.data.list[0].main.temp
-        //Gets the current sky conditions
-        const currentDayPrecipitation = res.data.list[0].weather[0].main
-        // Gets the current humidity level
-        const currentDayHumidity = res.data.list[0].main.humidity
-        //Gets the current wind speed
-        const currentDayWind = res.data.list[0].wind.speed
-        //Gets the current day's low temperature
-        const currentDayLow = res.data.list[0].main.temp_min
-        //Gets the current day's high temperature
-        const currentDayHigh = res.data.list[0].main.temp_max
 
-        //The following methods just input the variable values into the document
+		//Gets the current temperature
+		const currentDayTemp = Math.round(res.data.list[0].main.temp);
+		//Gets the current sky conditions
+		const currentDayPrecipitation = res.data.list[0].weather[0].main;
+		// Gets the current humidity level
+		const currentDayHumidity = res.data.list[0].main.humidity;
+		//Gets the current wind speed
+		const currentDayWind = res.data.list[0].wind.speed;
+		//Gets the current day's low temperature
+		const currentDayLow = Math.round(res.data.list[0].main.temp_min);
+		//Gets the current day's high temperature
+		const currentDayHigh = Math.round(res.data.list[0].main.temp_max);
 
-        //The API lists temperatures per 3 hour interval, meaning 8 indexes per day, so each day's index list retrieval increments by 8, then the temperature is rounded.
-        const secondDayTemp = Math.round(res.data.list[8].main.temp)
-        const thirdDayTemp = Math.round(res.data.list[16].main.temp)
-        const fourthDayTemp = Math.round(res.data.list[24].main.temp)
-        const fifthDayTemp = Math.round(res.data.list[32].main.temp)
+		//The following methods just input the variable values into the document
 
-        //This long-winded series of code blocks just caluate the date using unix timecodes, converts that date into a string, splits it into an array, then takes the first index of that array, which is the three-letter day abbreviation.
-        let secondDayDate = new Date(res.data.list[8].dt*1000);
-        let dateToString = secondDayDate.toString();
-        let dateToArray = dateToString.split(" ")
-        let secondDayName = dateToArray[0]
+		//The API lists temperatures per 3 hour interval, meaning 8 indexes per day, so each day's index list retrieval increments by 8, then the temperature is rounded.
+		const secondDayTemp = Math.round(res.data.list[8].main.temp);
+		const thirdDayTemp = Math.round(res.data.list[16].main.temp);
+		const fourthDayTemp = Math.round(res.data.list[24].main.temp);
+		const fifthDayTemp = Math.round(res.data.list[32].main.temp);
 
-        let thirdDayDate = new Date(res.data.list[16].dt*1000);
-        dateToString = thirdDayDate.toString();
-        dateToArray = dateToString.split(" ")
-        let thirdDayName = dateToArray[0]
-        
-        let fourthDayDate = new Date(res.data.list[24].dt*1000);
-        dateToString = fourthDayDate.toString();
-        dateToArray = dateToString.split(" ")
-        let fourthDayName = dateToArray[0]
-        
-        let fifthDayDate = new Date(res.data.list[32].dt*1000);
-        dateToString = fifthDayDate.toString();
-        dateToArray = dateToString.split(" ")
-		let fifthDayName = dateToArray[0]
+		//This long-winded series of code blocks just caluate the date using unix timecodes, converts that date into a string, splits it into an array, then takes the first index of that array, which is the three-letter day abbreviation.
+		let secondDayDate = new Date(res.data.list[8].dt * 1000);
+		let dateToString = secondDayDate.toString();
+		let dateToArray = dateToString.split(" ");
+		let secondDayName = dateToArray[0];
+
+		let thirdDayDate = new Date(res.data.list[16].dt * 1000);
+		dateToString = thirdDayDate.toString();
+		dateToArray = dateToString.split(" ");
+		let thirdDayName = dateToArray[0];
+
+		let fourthDayDate = new Date(res.data.list[24].dt * 1000);
+		dateToString = fourthDayDate.toString();
+		dateToArray = dateToString.split(" ");
+		let fourthDayName = dateToArray[0];
+
+		let fifthDayDate = new Date(res.data.list[32].dt * 1000);
+		dateToString = fifthDayDate.toString();
+		dateToArray = dateToString.split(" ");
+		let fifthDayName = dateToArray[0];
+
+		this.setState({
+			location: "Atlanta",
+			currentTemp: currentDayTemp,
+			currentPrecip: currentDayPrecipitation,
+			currentHumid: currentDayHumidity,
+			currentHigh: currentDayHigh,
+			currentLow: currentDayLow,
+			currentWind: currentDayWind,
+			secondDayTemp: secondDayTemp,
+			thirdDayTemp: thirdDayTemp,
+			fourthDayTemp: fourthDayTemp,
+			fifthDayTemp: fifthDayTemp,
+			secondDayName: secondDayName,
+			thirdDayName: thirdDayName,
+			fourthDayName: fourthDayName,
+			fifthDayName: fifthDayName
+		});
 	};
-
 
 	render() {
 		return (
@@ -69,28 +99,45 @@ class WeatherBox extends React.Component {
 						<tbody>
 							<tr>
 								<td className="has-text-centered" colSpan="4">
-									City Name
+									{this.state.location}
 								</td>
 							</tr>
 							<tr>
-								<td rowSpan="4">Current Temp</td>
-								<td>50</td>
+								<td rowSpan="4">
+									Current Temp: <br />
+									{this.state.currentTemp}°F
+								</td>
+								<td>
+									Sky Conditions: {this.state.currentPrecip}{" "}
+								</td>
+							</tr>
+							<tr>
+								<td>Humidity: {this.state.currentHumid}% </td>
+							</tr>
+							<tr>
+								<td>Wind: {this.state.currentWind}mph</td>
+							</tr>
+							<tr>
+								<td>
+									High/Low: {this.state.currentHigh}°/
+									{this.state.currentLow}°
+								</td>
 							</tr>
 						</tbody>
 					</table>
 					<table className="table is-bordered is-hoverable">
 						<tbody>
 							<tr>
-								<td>Monday</td>
-								<td>Tuesday</td>
-								<td>Wednesday</td>
-								<td>Thursday</td>
+								<td>{this.state.secondDayName}</td>
+								<td>{this.state.thirdDayName}</td>
+								<td>{this.state.fourthDayName}</td>
+								<td>{this.state.fifthDayName}</td>
 							</tr>
 							<tr>
-	<td>{this.onImageClick.tomato}</td>
-								<td>10</td>
-								<td>10</td>
-								<td>10</td>
+								<td>{this.state.secondDayTemp}°</td>
+								<td>{this.state.thirdDayTemp}°</td>
+								<td>{this.state.fourthDayTemp}°</td>
+								<td>{this.state.fifthDayTemp}°</td>
 							</tr>
 						</tbody>
 					</table>
